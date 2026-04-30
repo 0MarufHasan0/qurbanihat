d5b0ce37811cf78a9c92431ebff4c90457091a17
0MarufHasan0 <marufhasan11.11.2323@gmail.com>
2026-05-01 02:55:19 +0600

    AnimalCard added

--- src/components/AnimalCard.jsx
+++ src/components/AnimalCard.jsx
@@ -4,43 +4,51 @@ import Link from 'next/link';
 import React from 'react';
 
 const AnimalCard = ({ animal }) => {
-  console.log("animal", animal);
-
   return (
-    <Card className='border  rounded-2xl overflow-hidden shadow-sm hover:shadow-xl transition-all duration-300'>
+    <Card className='border rounded-2xl overflow-hidden shadow-sm hover:shadow-xl transition-all duration-300'>
 
-      <div className='relative w-full aspect-square bg-gray-100'>
-       <Image
-  src={animal?.photo}
-  alt="animal"
-  width={300}
-  height={300}
-/>
+      <div className='relative w-full h-44 bg-gray-100 overflow-hidden'>
+        
+        <Image
+          src={animal?.photo }
+          fill
+          alt={animal?.name}
+          className='object-cover hover:scale-105 transition-transform duration-300'
+        />
+
         <Chip 
-          size='sm' 
-          className='absolute top-3 right-3 bg-black/60 text-white'
-        >
+          size='xl' 
+          className='absolute  top-2 right-2 bg-black/60 text-white text-xs'>
           {animal?.breed}
         </Chip>
+
       </div>
-      <div className='p-4 space-y-2'>
+
+   
+      <div className='p-4 space-y-3'>
         
-        <h2 className='font-semibold text-lg text-gray-800'>
+        <h2 className='font-semibold text-base text-gray-800'>
           {animal?.name} - {animal?.type}
         </h2>
 
-        <div className='flex  font-bold items-center gap-10 text-sm text-gray-500'>
-          <p>Weight : <span className='text-green-600'> {animal?.weight} Kg </span></p>
-          <p>Age : <span className='text-green-600'> {animal?.age} Years</span></p>
-        </div>
+        <p className='text-gray-500 font-medium'>{animal.description}</p>
+
+    
+          <p>
+            Weight : <span className='text-green-600 font-semibold'>{animal?.weight} Kg</span>
+          </p>
+        
+        
 
         <p className='text-green-600 font-bold text-lg'>
           ৳ {animal?.price} Taka
         </p>
+
         <Link href={'/'}>
           <Button 
             variant='outline' 
-            className='w-full mt-2 border-green-600 text-green-600 hover:bg-green-600 hover:text-white transition'>
+            className='w-full border-green-600 text-green-600 hover:bg-green-600 hover:text-white transition'
+          >
             View Details
           </Button>
         </Link>
